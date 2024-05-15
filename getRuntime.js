7bccc27cecd88584a126c7d711ae89662931729e
Luan Himmlisch <luan@himmlisch.com.mx>
2024-05-15 00:32:16 -0600

    Added getRuntime.js

---
+++ getRuntime.js
@@ -0,0 +1,31 @@
+if(globalThis._C3_InitRuntime === undefined){
+	globalThis._C3_InitRuntime = self.C3_InitRuntime;
+
+	self.C3_InitRuntime = async function (...args){
+		const promise = globalThis._C3_InitRuntime(...args);	
+		promise.then(v => {
+			globalThis._C3_RealRuntime = v;
+			return v;
+		});
+		return promise;
+	}
+}
+
+async function getRuntime(){
+	if(globalThis._C3_RealRuntime !== undefined){
+		return globalThis._C3_RealRuntime;
+	}
+
+	return new Promise(resolve => {
+		let interval;
+		
+		interval = setInterval(() => {
+			if (globalThis._C3_RealRuntime !== undefined) {
+				clearInterval(interval);
+				resolve(globalThis._C3_RealRuntime);
+			}
+		}, 100);
+	});
+}
+
+export default getRuntime;
